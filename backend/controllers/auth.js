d63bf4f632314711ee4ba6dcf194b6e3affb68dc
seohyun <ttony07090709@gmail.com>
2025-03-11 13:36:06 +0900

    login redirection

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,4 +1,5 @@
 import { login as authServiceLogin, signup as authServiceSignUp} from '../services/auth.js';
+import { nodeEnv } from '../config/env.js'
 
 // 클라이언트로부터 받은 로그인 요청 처리 함수
 export async function login(req, res) {
@@ -7,14 +8,13 @@ export async function login(req, res) {
   try {
     // 서비스 계층에서 로그인 로직 수행
     const token = await authServiceLogin(email, password);
-
     // 로그인 실패 (예: 유효하지 않은 자격증명)
     if (!token) {
       return res.status(401).json({ message: '로그인 실패: 유효하지 않은 자격 증명' });
     }
-
     // 로그인 성공
-    res.status(200).json({ message: '로그인 성공', token });
+    res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
+    return res.redirect('/');
   } catch (error) {
     // 예외 발생 시 서버 에러 응답
     res.status(500).json({ message: '서버 에러', error: error.message });
